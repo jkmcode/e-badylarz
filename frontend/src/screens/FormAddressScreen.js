55e7d144dc557ea6be6e6e29caa1e894df13f3d1
jkmcode <jakub.kumala@jkmcode.eu>
2022-07-19 19:53:15 +0200

    StartPage-css

--- frontend/src/screens/FormAddressScreen.js
+++ frontend/src/screens/FormAddressScreen.js
@@ -68,120 +68,152 @@ function FormAddressScreen() {
     }
   }, [dispatch, districtList.length]);
 
-  // test
-  const myStyle = {
+  // Style
+  const backgroungImage = {
     backgroundImage: `url(${background})`,
-    height: "100vh",
-    marginTop: "-70px",
-    fontSize: "50px",
+    height: "120vh",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
   };
 
+  const shadowTransparentParent = {
+    position: "relative",
+  };
+
+  const shadowTransparentChildren = {
+    backgroundColor: "black",
+    height: "20vh",
+    width: "100%",
+    opacity: "40%",
+    marginTop: "-20vh",
+  };
+
+  const topright = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  };
+
   return (
-    <div className="mt-2" style={myStyle}>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <p>error</p>
-      ) : (
-        // <div className="container bg-container max-sizing mt-5 p-4 rounded">
-        <div className="container bg-container max-sizing p-4 rounded">
-          <Row>
-            <Col>
-              <h5 className="text-center">{t("FormAddressScreen_title")}</h5>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <h5 className="text-center">{t("FormAddressScreen_subtitle")}</h5>
-            </Col>
-          </Row>
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group>
-              <Row>
-                <Col>
-                  <Form.Select
-                    name="district"
-                    {...register("district")}
-                    onChange={selectHandler}
-                  >
-                    <option key="blankChoice" hidden value={zero}>
-                      {t("FormAddressScreen__district_placeholder")}
-                    </option>
-                    {districtList.map((i) => (
-                      <option key={i.id}>{i.name}</option>
-                    ))}
-                  </Form.Select>
-                  {districRequired ? <p>{districRequired}</p> : null}
-                </Col>
-              </Row>
-            </Form.Group>
-            <Form.Group>
-              <Row>
-                <Col>
-                  <Form.Select
-                    //aria-label="Default select example"
-                    name="city"
-                    {...register("city")}
-                    disabled={disabledField}
-                    className="mt-3"
-                    onChange={selectHandler2}
-                    defaultValue="0"
-                  >
-                    <option key="blankChoice" hidden value={zero}>
-                      {t("FormAddressScreen__city_placeholder")}
-                    </option>
-                    <option key="zeroValue" value={zero}></option>
-                    <option>{cities[0]}</option>
-                    <option>{cities[1]}</option>
-                    <option>{cities[2]}</option>
-                  </Form.Select>
-                </Col>
-              </Row>
-              {locationMsg ? <p>{locationMsg}</p> : null}
-            </Form.Group>
+    <>
+      <div style={backgroungImage}>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <p>error</p>
+        ) : (
+          // <div className="container bg-container max-sizing mt-5 p-4 rounded">
+          <div className="container bg-container max-sizing p-4 rounded">
             <Row>
-              <Col className="text-center mt-4 color-title">
-                <p className="test3">lub</p>
+              <Col>
+                <h5 className="text-center">{t("FormAddressScreen_title")}</h5>
               </Col>
             </Row>
-            <Form.Group>
+            <Row>
+              <Col>
+                <h5 className="text-center">
+                  {t("FormAddressScreen_subtitle")}
+                </h5>
+              </Col>
+            </Row>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Form.Group>
+                <Row>
+                  <Col>
+                    <Form.Select
+                      name="district"
+                      {...register("district")}
+                      onChange={selectHandler}
+                    >
+                      <option key="blankChoice" hidden value={zero}>
+                        {t("FormAddressScreen__district_placeholder")}
+                      </option>
+                      {districtList.map((i) => (
+                        <option key={i.id}>{i.name}</option>
+                      ))}
+                    </Form.Select>
+                    {districRequired ? <p>{districRequired}</p> : null}
+                  </Col>
+                </Row>
+              </Form.Group>
+              <Form.Group>
+                <Row>
+                  <Col>
+                    <Form.Select
+                      //aria-label="Default select example"
+                      name="city"
+                      {...register("city")}
+                      disabled={disabledField}
+                      className="mt-3"
+                      onChange={selectHandler2}
+                      defaultValue="0"
+                    >
+                      <option key="blankChoice" hidden value={zero}>
+                        {t("FormAddressScreen__city_placeholder")}
+                      </option>
+                      <option key="zeroValue" value={zero}></option>
+                      <option>{cities[0]}</option>
+                      <option>{cities[1]}</option>
+                      <option>{cities[2]}</option>
+                    </Form.Select>
+                  </Col>
+                </Row>
+                {locationMsg ? <p>{locationMsg}</p> : null}
+              </Form.Group>
               <Row>
-                <Col>
-                  <Form.Select
-                    name="pickupLocation"
-                    {...register("pickupLocation")}
-                    disabled={disabledField}
-                    className="mt-3"
-                    onChange={selectHandler2}
-                  >
-                    <option key="blankChoice" hidden value={zero}>
-                      {t("FormAddressScreen__pickup_location_placeholder")}
-                    </option>
-                    <option key="zeroValue" value={zero}></option>
-                    <option>{cities[0]}</option>
-                    <option>{cities[1]}</option>
-                    <option>{cities[2]}</option>
-                  </Form.Select>
+                <Col className="text-center mt-4 color-title">
+                  <p className="test3">lub</p>
                 </Col>
               </Row>
-              {locationMsg ? <p>{locationMsg}</p> : null}
-            </Form.Group>
-            <Row>
-              <h5 className="mt-3 ">{t("FormAddressScreen__info")}</h5>
-            </Row>
-            <Button
-              type="submit"
-              variant="success"
-              className="rounded my-3 w-100"
-            >
-              {t("FormAddressScreen__btn")}
-            </Button>
-          </Form>
-        </div>
-      )}
-    </div>
+              <Form.Group>
+                <Row>
+                  <Col>
+                    <Form.Select
+                      name="pickupLocation"
+                      {...register("pickupLocation")}
+                      disabled={disabledField}
+                      className="mt-3"
+                      onChange={selectHandler2}
+                    >
+                      <option key="blankChoice" hidden value={zero}>
+                        {t("FormAddressScreen__pickup_location_placeholder")}
+                      </option>
+                      <option key="zeroValue" value={zero}></option>
+                      <option>{cities[0]}</option>
+                      <option>{cities[1]}</option>
+                      <option>{cities[2]}</option>
+                    </Form.Select>
+                  </Col>
+                </Row>
+                {locationMsg ? <p>{locationMsg}</p> : null}
+              </Form.Group>
+              <Row>
+                <h5 className="mt-3 ">{t("FormAddressScreen__info")}</h5>
+              </Row>
+              <Button
+                type="submit"
+                variant="success"
+                className="rounded my-3 w-100"
+              >
+                {t("FormAddressScreen__btn")}
+              </Button>
+            </Form>
+          </div>
+        )}
+      </div>
+      <div style={shadowTransparentParent}>
+        <div style={shadowTransparentChildren} />
+        <Button
+          type="submit"
+          variant="success"
+          className="rounded w-30"
+          style={topright}
+        >
+          {t("FormAddressScreen__learn_more_btn")}
+        </Button>
+      </div>
+    </>
   );
 }
 
