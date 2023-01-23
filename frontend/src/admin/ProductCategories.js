de049e77f799d2134f6f76e6869ac58f5c545e3d
Jakub Kumala <jkmcode2022@gmail.com>
2023-01-23 19:45:15 +0100

    Update 1.1 ProductCategories

--- frontend/src/admin/ProductCategories.js
+++ frontend/src/admin/ProductCategories.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { title, FormLayout } from "./AdminCSS";
+import { title, FormLayout, submitBtn } from "./AdminCSS";
 import { dataProductCategories } from "../Data/dataProductCategories";
+import { ONE } from "../constants/adminConstans";
+import RadioButtons from "./RadioButtons";
 
 function ProductCategories() {
   const { t } = useTranslation();
 
-  console.log("dataProductCategories", dataProductCategories);
+  const radios = [
+    { id: 1, name: t("Radio_true"), value: "1" },
+    { id: 2, name: t("Radio_false"), value: "0" },
+  ];
+
+  //variables
+  const [radioValue, setRadioValue] = useState(ONE);
+
+  //RadioButtons functions
+  const handleBtnValue = (e) => {
+    setRadioValue(e.target.value);
+  };
 
   return (
     <div
@@ -17,30 +30,71 @@ function ProductCategories() {
         borderRadius: "0.5rem",
       }}
     >
+      <RadioButtons handleBtnValue={handleBtnValue} radios={radios} />
       <div style={title}>{t("ProductCategories_title")}</div>
-
-      <FormLayout col="3">
-        {dataProductCategories.map((category) => {
-          return (
-            <button
-              style={{
-                backgroundImage:
-                  "linear-gradient(195deg, rgb(66, 66, 74) 0%, rgb(25, 25, 25) 100%)",
-                color: "white",
-                minWidth: "200px",
-                minHeight: "200px",
-                fontSize: "2rem",
-                borderRadius: "1rem",
-                margin: "auto",
-                marginBottom: "1rem",
-              }}
-            >
-              {category.name}
-              <div>{category.icon}</div>
-            </button>
-          );
-        })}
-      </FormLayout>
+      <div
+        style={{
+          padding: "1rem",
+          borderRadius: "1rem",
+          border: "3px solid rgb(66, 66, 74)",
+        }}
+      >
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {dataProductCategories.map((category) => {
+            if (category.isActive && radioValue === "1") {
+              return (
+                <button
+                  style={{
+                    backgroundImage:
+                      "linear-gradient(195deg, rgb(66, 66, 74) 0%, rgb(25, 25, 25) 100%)",
+                    color: "white",
+                    minWidth: "200px",
+                    minHeight: "200px",
+                    fontSize: "2rem",
+                    borderRadius: "1rem",
+                    margin: "auto",
+                    marginBottom: "1rem",
+                  }}
+                >
+                  {category.name}
+                  <div>{category.icon}</div>
+                </button>
+              );
+            }
+            if (!category.isActive && radioValue === "0") {
+              return (
+                <button
+                  style={{
+                    backgroundImage:
+                      "linear-gradient(195deg, rgb(66, 66, 74) 0%, rgb(25, 25, 25) 100%)",
+                    color: "white",
+                    minWidth: "200px",
+                    minHeight: "200px",
+                    fontSize: "2rem",
+                    borderRadius: "1rem",
+                    margin: "auto",
+                    marginBottom: "1rem",
+                  }}
+                >
+                  {category.name}
+                  <div>{category.icon}</div>
+                </button>
+              );
+            }
+          })}
+        </div>
+      </div>
+      {radioValue === ONE && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "1rem",
+          }}
+        >
+          <button style={submitBtn}>Add Category</button>
+        </div>
+      )}
     </div>
   );
 }
